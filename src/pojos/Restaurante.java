f0d44dbc530fdd5ceaf1705f996f2e71de9c72b6
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-06-16 16:31:08 -0300

    fix: adicionando alterações aos pojos para trabalhar com json

--- src/pojos/Restaurante.java
+++ src/pojos/Restaurante.java
@@ -1,4 +1,5 @@
 package pojos;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,18 +7,32 @@ public class Restaurante {
     private String nome;
     private List<Pedido> pedidos;
 
+    public Restaurante() {
+        pedidos = new ArrayList<>();
+    }
+
     public Restaurante(String nome) {
         this.nome = nome;
         this.pedidos = new ArrayList<>();
     }
 
-    public void adicionarPedido(Pedido pedido) {
-        pedidos.add(pedido);
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public List<Pedido> getPedidos() {
         return pedidos;
     }
 
-    public String getNome() { return nome; }
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+
+    public void adicionarPedido(Pedido pedido) {
+        pedidos.add(pedido);
+    }
 }
